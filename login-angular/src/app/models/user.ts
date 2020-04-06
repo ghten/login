e4040ccd2a08deb09465a945ae574b31c11052f2
ghten <45689669+ghten@users.noreply.github.com>
2020-04-06 16:38:26 +0200

    Add files via upload

---
+++ login-angular/src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    public id: number;
+    public name: string;
+    public email: string;
+    public password: string;
+    // tslint:disable-next-line: variable-name
+    public created_at: string;
+    // tslint:disable-next-line: variable-name
+    public updated_at: string;
+ }
